a9b950bf9a7768579cbb1bd852e08a82acf79e32
Vladimir Maslovskij <maslovskij.98@mail.ru>
2022-09-03 13:21:31 +0300

    gcHomeWork

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -1,10 +1,7 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class GarbageCollectorImplementation implements GarbageCollector {
@@ -12,8 +9,42 @@ public class GarbageCollectorImplementation implements GarbageCollector {
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Map<String, ApplicationBean> beans = heap.getBeans();
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return new ArrayList<>();
+    Set<ApplicationBean> aliveBeans = getAliveBeans(frames);
+    List<ApplicationBean> deadBeans = getDeadBeans(beans, aliveBeans);
+
+    return deadBeans;
   }
 
+  public List<ApplicationBean> getDeadBeans(Map<String, ApplicationBean> beans, Set<ApplicationBean> aliveBeans){
+    List<ApplicationBean> deadBeans = new ArrayList<>();
+    Set<ApplicationBean> allBeans = new HashSet<>();
+    for(Map.Entry<String, ApplicationBean> beanEntry : beans.entrySet()){
+      allBeans.add(beanEntry.getValue());
+    }
+    allBeans.removeAll(aliveBeans);
+    for(ApplicationBean bean : allBeans)
+      deadBeans.add(bean);
+    return deadBeans;
+  }
+
+  public Set<ApplicationBean> getAliveBeans(Deque<StackInfo.Frame> frames){
+    Set<ApplicationBean> aliveBeans = new HashSet<>();
+    for(StackInfo.Frame frame : frames){
+      for (ApplicationBean bean : frame.parameters) {
+        aliveBeans.addAll(getRelation(bean, aliveBeans));
+      }
+    }
+    return aliveBeans;
+  }
+
+  private Set<ApplicationBean> getRelation(ApplicationBean bean, Set<ApplicationBean> beanSet) {
+    if (!beanSet.contains(bean)){
+      beanSet.add(bean);
+      for(Map.Entry<String, ApplicationBean> beanEntry : bean.getFieldValues().entrySet()){
+        getRelation(beanEntry.getValue(), beanSet);
+      }
+    }
+    return beanSet;
+  }
 }
 
