e65a2a9f43d6073b268aff7b96e00aab2e4bde66
Vladimir Semkin <vnsemkin@gmail.com>
2022-09-04 13:50:50 +0300

    GB version 2. Revised. Special thanks ilya.taslitsky@gmail.com.

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -1,47 +1,45 @@
 package org.example;
-
 import java.util.*;
 
-
 public class GarbageCollectorImplementation implements GarbageCollector {
+
   @Override
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Map<String, ApplicationBean> beans = heap.getBeans();
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return getGarbage(getStack(frames),getHeap(beans));
+    return getGarbage(getAliveBeans(frames),getBeans(beans));
   }
-  public Set<ApplicationBean> getStack(Deque<StackInfo.Frame> frames){
-    Set<ApplicationBean> stackSet = new HashSet<>();
+
+  public Set<ApplicationBean> getBeans( Map<String, ApplicationBean> beans) {
+    Set<ApplicationBean> beansSet = new HashSet<>();
+    for (Map.Entry<String, ApplicationBean> beanEntry : beans.entrySet()) {
+        beansSet.add(beanEntry.getValue());
+    }
+    return beansSet;
+  }
+
+  public Set<ApplicationBean> getAliveBeans(Deque<StackInfo.Frame> frames) {
+    Set<ApplicationBean> aliveBeansSet = new HashSet<>();
     for (StackInfo.Frame frame : frames) {
       for (ApplicationBean applicationBean : frame.getParameters()) {
-        stackSet = getStackChild(applicationBean, stackSet);
+        aliveBeansSet = getChild(applicationBean, aliveBeansSet);
       }
     }
-    return stackSet;
+    return aliveBeansSet;
   }
-  public Set<ApplicationBean> getHeap( Map<String, ApplicationBean> beans){
-    Set<ApplicationBean> heapSet = new HashSet<>();
-    for (Map.Entry<String, ApplicationBean> beanEntry : beans.entrySet()) {
-        heapSet.add(beanEntry.getValue());
-    }
-    return heapSet;
-  }
-  public List<ApplicationBean> getGarbage(Set<ApplicationBean> stack, Set<ApplicationBean> heap) {
-    List<ApplicationBean> garbage = new ArrayList<>();
-    for (ApplicationBean heapBean : heap) {
-      if (!stack.contains(heapBean)) {
-        garbage.add(heapBean);
-      }
-    }
-    return garbage;
-  }
-  public Set<ApplicationBean> getStackChild(ApplicationBean bean, Set<ApplicationBean> stackSet){
+
+  public Set<ApplicationBean> getChild(ApplicationBean bean, Set<ApplicationBean> stackSet) {
     stackSet.add(bean);
       for (ApplicationBean applicationBean : bean.getFieldValues().values()) {
         if (!stackSet.contains(applicationBean))
-          stackSet.addAll(getStackChild(applicationBean, stackSet));
+          stackSet.addAll(getChild(applicationBean, stackSet));
       }
     return stackSet;
+  }
+
+  public List<ApplicationBean> getGarbage(Set<ApplicationBean> stack, Set<ApplicationBean> heap) {
+    heap.removeAll(stack);
+    return new ArrayList<>(heap);
   }
 }
 
