06312c07de6380a3883c252de1ad0070261fbb74
Sergoigo <111062392+Sergoigo@users.noreply.github.com>
2022-09-06 17:54:52 +0300

    Update GarbageCollectorImplementation.java

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -1,10 +1,9 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+
+
+import java.util.*;
 
 
 public class GarbageCollectorImplementation implements GarbageCollector {
@@ -12,8 +11,46 @@ public class GarbageCollectorImplementation implements GarbageCollector {
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Map<String, ApplicationBean> beans = heap.getBeans();
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return new ArrayList<>();
+    Set<ApplicationBean> aliveBeans = getAliveBeans(frames);
+    Set<ApplicationBean> aliveHeapBeans = getAliveBeansFromHeap(beans);
+    ArrayList<ApplicationBean> garbageList = new ArrayList<>();
+    for (ApplicationBean bean : aliveHeapBeans) {
+      if (!aliveBeans.contains(bean)){
+        garbageList.add(bean);
+      }
+    }
+    return garbageList;
   }
 
-}
+  public Set<ApplicationBean> getAliveBeans(Deque<StackInfo.Frame> frames) {
+    Set<ApplicationBean> aliveBeans = new HashSet<>();
+    for (StackInfo.Frame frame :
+            frames) {
+      for (ApplicationBean bean :
+              frame.parameters) {
+        aliveBeans.addAll(getRelation(bean, aliveBeans));
+      }
+    }
+    return aliveBeans;
+  }
 
+  public Set<ApplicationBean> getAliveBeansFromHeap(Map<String, ApplicationBean> beans) {
+    Set<ApplicationBean> aliveBeans = new HashSet<>();
+    for (Map.Entry<String, ApplicationBean> bean :
+            beans.entrySet()) {
+      ApplicationBean frame = bean.getValue();
+        aliveBeans.addAll(getRelation(frame, aliveBeans));
+    }
+    return aliveBeans;
+  }
+  private Set<ApplicationBean> getRelation(ApplicationBean bean, Set<ApplicationBean> beanSet) {
+    if (!beanSet.contains(bean)){
+      beanSet.add(bean);
+      for(Map.Entry<String, ApplicationBean> beanEntry : bean.getFieldValues().entrySet()){
+        getRelation(beanEntry.getValue(), beanSet);
+      }
+    }
+    return beanSet;
+
+  }
+}
