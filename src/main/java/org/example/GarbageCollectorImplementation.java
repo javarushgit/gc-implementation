e80625c2d84712a9fa6737f03a7ffdee1152f744
Crakozyabra <mrcrakozyabra@gmail.com>
2022-11-27 14:43:43 +0300

    commit for complete

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -1,18 +1,46 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class GarbageCollectorImplementation implements GarbageCollector {
   @Override
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
-    Map<String, ApplicationBean> beans = heap.getBeans();
+
+    Set<ApplicationBean> heapApplicationBeans = new LinkedHashSet<>(heap.getBeans().values());
+    Set<ApplicationBean> stackApplicationBeans = new LinkedHashSet<>();
+
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return new ArrayList<>();
+
+    for (StackInfo.Frame frame: frames) {
+      stackApplicationBeans.addAll(getFrameBeans(frame));
+    }
+    heapApplicationBeans.removeAll(stackApplicationBeans);
+    return new ArrayList<>(heapApplicationBeans);
+  }
+
+  private Set<ApplicationBean> getFrameBeans(StackInfo.Frame frame){
+    List<ApplicationBean> frameApplicationBeans = frame.getParameters();
+    Set<ApplicationBean> allFrameApplicationBeans = new HashSet<>();
+    Set<ApplicationBean> childrenBeans = new HashSet<>();
+    for (ApplicationBean applicationBean:frameApplicationBeans) {
+      allFrameApplicationBeans.addAll(getChildren(childrenBeans, applicationBean));
+    }
+    return allFrameApplicationBeans;
+  }
+
+  private Set<ApplicationBean> getChildren(Set<ApplicationBean> childrenBeans, ApplicationBean bean) {
+    if (!childrenBeans.contains(bean)) {
+        childrenBeans.add(bean);
+        bean.getFieldValues()
+            .forEach(
+                    (key, value) -> {
+                        childrenBeans.addAll(getChildren(childrenBeans, value));
+                      }
+                    );
+    }
+    return childrenBeans;
   }
 
 }
